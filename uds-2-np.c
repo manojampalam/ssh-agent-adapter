36115cb76e28e67a4e292376346f023cbdfba810
Manoj Ampalam <manojamp@microsoft.com>
2018-04-11 14:51:56 -0700

    C6

--- uds-2-np.c
+++ uds-2-np.c
@@ -1,5 +1,6 @@
 #include "common.h"
 
+int uds_agent_port = 47010;
 
 void process_pipe_connection(connection* con) {
 	WSADATA wsaData;
@@ -9,10 +10,12 @@ void process_pipe_connection(connection* con) {
 	con->sock = socket(AF_INET, SOCK_STREAM, 0);
 	ZeroMemory(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(47010);
+	serv_addr.sin_port = htons(uds_agent_port);
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	connect(con->sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+	//TODO - write cookie
 
 	//start threads
 	con->activity_count = 2;
@@ -24,6 +27,31 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 {
 	wchar_t pipe_name[MAX_PATH];
 	OVERLAPPED ol;
+
+	//spawn a child go give back control to cmd
+	if (argc == 1) {
+		char cmdline[MAX_PATH];
+		STARTUPINFOA si;
+		PROCESS_INFORMATION pi;
+
+		//read SSH_AUTH_SOCK
+		wchar_t* auth_sock = "filepath";
+		auth_sock = getenv("SSH_AUTH_SOCK");
+		cmdline[0] = '\0';
+		strcat(cmdline, "E:\\temp\\sample.exe");
+		strcat(cmdline, " ");
+		strcat(cmdline, auth_sock);
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(STARTUPINFO);
+		ZeroMemory(&pi, sizeof(pi));
+		CreateProcessA(NULL, cmdline, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+		swprintf_s(pipe_name, MAX_PATH, L"SSH_AUTH_SOCK=\\\\.\\pipe\\usd-2-np-%d", pi.dwProcessId);
+		_wputenv(pipe_name);
+		printf("%ls", pipe_name);
+	}
+
+	//TODO - parse contents of file pointed by argv[1]
+	// and read port and cookie info
 
 	swprintf_s(pipe_name, MAX_PATH, L"\\\\.\\pipe\\usd-2-np-%d", GetCurrentProcessId());
 	ZeroMemory(&ol, sizeof(ol));
