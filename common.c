41a27f5c2489565ad698d0fa57cec60a55ab6519
Chris Charabaruk <chris.charabaruk@northcloud.com>
2018-04-20 14:47:33 -0400

    common: use `log()` macro for debugging statements
    Also make nonzero check explicit for `debug_mode`  in the macro to
    ensure that messages _will_ be printed when the debug flag is used.

--- common.c
+++ common.c
@@ -9,9 +9,7 @@ static void con_activity_done(connection* con) {
 		CloseHandle(con->np2uds_th);
 		CloseHandle(con->uds2np_th);
 		free(con);
-		if (debug_mode != 0) {
-			printf("connection closed\n");
-		}
+		log("connection closed");
 	}
 }
 
@@ -36,45 +34,33 @@ DWORD WINAPI uds2np_thread(LPVOID param) {
 		ZeroMemory(&ol, sizeof(OVERLAPPED));
 		// read from pipe
 		if (!ReadFileEx(con->pipe, buf, BUFSIZE, &ol, apc_callback)) {
-			if (debug_mode != 0) {
-				printf("couldn't start read from pipe\n");
-			}
+			log("couldn't start read from pipe");
 			goto done;
 		}
 		
 		SleepEx(INFINITE, TRUE);
 		bytes_read = 0;
 		if (con->abort_io || !GetOverlappedResult(con->pipe, &ol, &bytes_read, FALSE)) {
-			if (debug_mode != 0) {
-				printf("couldn't get bytes from pipe, or read aborted\n");
-			}
+			log("couldn't get bytes from pipe, or read aborted");
 			goto done;
 		}
-		if (debug_mode != 0) {
-			printf("read %d bytes from pipe\n", bytes_read);
-		}
+		log("read %d bytes from pipe", bytes_read);
 
 		//now write to sock
 		bytes_written = 0;
 		while (bytes_written < bytes_read) {
 			int written = send(con->sock, buf + bytes_written, bytes_read - bytes_written, 0);
 			if (written == SOCKET_ERROR) {
-				if (debug_mode != 0) {
-					printf("error when writing to socket\n");
-				}
+				log("error when writing to socket");
 				goto done;
 			}
 			bytes_written += written;
-			if (debug_mode != 0) {
-				printf("wrote %d (%d) of %d bytes to socket\n", written, bytes_written, bytes_read);
-			}
+			log("wrote %d (%d) of %d bytes to socket", written, bytes_written, bytes_read);
 		}
 	}
 
 done:
-	if (debug_mode != 0) {
-		printf("shutting down socket\n");
-	}
+	log("shutting down socket");
 	shutdown(con->sock, SD_SEND);
 	con_activity_done(con);
 	return 0;
@@ -92,41 +78,29 @@ DWORD WINAPI np2uds_thread(LPVOID param) {
 		// read from sock
 		bytes_read = recv(con->sock, buf, BUFSIZE, 0);
 		if (bytes_read <= 0) {
-			if (debug_mode != 0) {
-				printf("nothing to read from socket\n");
-			}
+			log("nothing to read from socket");
 			goto done;
 		}
-		if (debug_mode != 0) {
-			printf("read %d bytes from socket\n", bytes_read);
-		}
+		log("read %d bytes from socket", bytes_read);
 
 		// now write to pipe
 		bytes_written = 0;
 		ZeroMemory(&ol, sizeof(OVERLAPPED));
 		if (!WriteFileEx(con->pipe, buf, bytes_read, &ol, apc_callback)) {
-			if (debug_mode != 0) {
-				printf("can't start write to pipe\n");
-			}
+			log("can't start write to pipe");
 			goto done;
 		}
 
 		SleepEx(INFINITE, TRUE);
 		if (!GetOverlappedResult(con->pipe, &ol, &bytes_written, FALSE)) {
-			if (debug_mode != 0) {
-				printf("couldn't write to pipe\n");
-			}
+			log("couldn't write to pipe");
 			goto done;
 		}
-		if (debug_mode != 0) {
-			printf("wrote %d of %d bytes to pipe\n", bytes_written, bytes_read);
-		}
+		log("wrote %d of %d bytes to pipe", bytes_written, bytes_read);
 	}
 
 done:
-	if (debug_mode != 0) {
-		printf("shutting down pipe\n");
-	}
+	log("shutting down pipe");
 	QueueUserAPC(apc_abortio, con->uds2np_th, (ULONG_PTR)con);
 	con_activity_done(con);
 	return 0;
